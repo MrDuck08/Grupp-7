4304b412dde73126972637bf1f146f893167692a
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-09-13 17:39:02 +0200

    Weapon Distance

--- Assets/Scripts/PlayerSword.cs
+++ Assets/Scripts/PlayerSword.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerSword : MonoBehaviour
@@ -9,6 +10,8 @@ public class PlayerSword : MonoBehaviour
 
     Transform beforeAttackTransform;
 
+    [SerializeField] Transform parentTransform;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -17,13 +20,6 @@ public class PlayerSword : MonoBehaviour
     void Update()
     {
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-
-
-
-        //Vector3 ss = transform.position - mousePos;
-
-        //transform.LookAt(ss);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -33,12 +29,28 @@ public class PlayerSword : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        //rb.AddForce(transform.right * 10 * Time.deltaTime);
+
+        Vector3 tr = parentTransform.position - transform.position;
+
+        float dis = Vector3.Distance(transform.position, parentTransform.position);
+
+        if (dis != 1.3f)
+        {
+            dis = 1.3f;
+            transform.position = (transform.position - parentTransform.position).normalized * dis + parentTransform.position;
+        }
+    }
+
     void Attack()
     {
         beforeAttackTransform = transform;
 
         Vector2 lookDirection = mousePos - rb.position;
-        transform.position = lookDirection * 100 * Time.deltaTime;
+
+
         
     }
 }
