d022c47a19ad79eca1c20c9826add8b038271458
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-11-09 12:06:56 +0100

    Continue with chip

--- Assets/Scripts/Chip.cs
+++ Assets/Scripts/Chip.cs
@@ -4,7 +4,18 @@ using UnityEngine.Rendering;
 public class Chip : MonoBehaviour
 {
 
-    float speed;
+    [Header("Wall & Ground check")]
+
+    [SerializeField] Vector2 wallCheckPosition;
+    [SerializeField] Vector2 groundCheckPosition;
+
+    [SerializeField] float checkRadius = 1f;
+
+    [SerializeField] LayerMask groundLayer;
+
+    [Header("Movement")]
+
+    [SerializeField] float speed = 3;
     [SerializeField] float walkRange = 3;
 
     bool stop = false;
@@ -19,7 +30,15 @@ public class Chip : MonoBehaviour
 
     }
 
-    private void Update()
+    private void FixedUpdate()
+    {
+
+        Movement();
+        Jump();
+
+    }
+
+    private void Movement()
     {
         float distanceFromPlayer = Vector2.Distance(player.position, transform.position);
 
@@ -28,9 +47,50 @@ public class Chip : MonoBehaviour
 
             float Direction = Mathf.Sign(player.position.x - transform.position.x);
 
-            Vector2 MovePos = new Vector2(transform.position.x + -Direction * speed * Time.deltaTime, transform.position.y);
+            Vector2 MovePos = new Vector2(transform.position.x + -Direction * (speed / distanceFromPlayer) * Time.deltaTime, transform.position.y);
+
+            transform.position = MovePos;
 
         }
+    }
+
+    void Jump()
+    {
+
+        Vector2 relativeWallCheckPosition = (Vector2)transform.position + new Vector2(wallCheckPosition.x, wallCheckPosition.y);
+        bool wallChecked = Physics2D.OverlapCircle(relativeWallCheckPosition, checkRadius, groundLayer);
+
+        Vector2 relativeGroundCheckPosition = (Vector2)transform.position + new Vector2(groundCheckPosition.x, groundCheckPosition.y);
+        bool groundChecked = Physics2D.OverlapCircle(relativeGroundCheckPosition, checkRadius, groundLayer);
+
+        if (wallChecked)
+        {
+
+            stop = true;
+            Debug.Log("STart Searching Up");
+            Vector2 relativeUpWallCheckPosition = (Vector2)transform.position + new Vector2(wallCheckPosition.x, wallCheckPosition.y + 0.01f * Time.deltaTime);
+            bool upWallChecked = Physics2D.OverlapCircle(relativeUpWallCheckPosition, checkRadius, groundLayer);
+
+            if (upWallChecked)
+            {
+                Debug.Log("Move Up");
+                transform.position = relativeWallCheckPosition;
+
+            }
+
+        }
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, walkRange);
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere((Vector2)transform.position + wallCheckPosition, checkRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + groundCheckPosition, checkRadius);
 
     }
 
